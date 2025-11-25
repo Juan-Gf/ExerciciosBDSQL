78a3277c7b0cc4588128a52f3bffa755bee4b71c
Juan G <juangustavo0508@gmail.com>
2025-11-25 16:02:33 -0300

    Exercicio 01 - Final

--- Exercicio01.sql
+++ Exercicio01.sql
@@ -16,5 +16,20 @@ select * from tb_colaboradores;
 insert into tb_colaboradores(nome, cargo, departamento, salario)
 values ("Juan", "Tecnico II", "TI", 2500.00);
 
+insert into tb_colaboradores(nome, cargo, departamento, salario)
+values ("Victor", "Gentente TI", "TI", 5500.00);
 
+insert into tb_colaboradores(nome, cargo, departamento, salario)
+values ("Paulo", "Asistente", "RH", 1500.00);
 
+insert into tb_colaboradores(nome, cargo, departamento, salario)
+values ("Mauro", "Cozinheiro", "Restaurante", 3060.00);
+
+insert into tb_colaboradores(nome, cargo, departamento, salario)
+values ("Tom", "Musico", "Restaurante", 1060.00);
+
+select * from tb_colaboradores where salario > 2000.00;
+
+select * from tb_colaboradores where salario < 2000.00;
+
+update tb_colaboradores set salario = 9000.00 where id = 2;
